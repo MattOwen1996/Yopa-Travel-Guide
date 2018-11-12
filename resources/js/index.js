f6c70aad9513b854889c3a24e3153405e8923925
MattOwen1996 <1996mattowen@gmail.com>
2018-11-12 01:19:41 +0000

    Pasted new maps js

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,238 +1,44 @@
-let map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
-
-
-google.load('visualization', '1', { packages: ['corechart'] });
-
-/**
- * Sector type mapped to a style rule.
- * @type {Object}
- * @const
- */
-const LAYER_STYLES = {
-  'Residential': {
-    'min': 0,
-    'max': 10000,
-    'colors': [
-      '#f4cccc',
-      '#ea9999',
-      '#e06666',
-      '#cc0000',
-      '#990000'
-    ]
-  },
-  'Non-Residential': {
-    'min': 0,
-    'max': 10000,
-    'colors': [
-      '#d0e0e3',
-      '#a2c4c9',
-      '#76a5af',
-      '#45818e',
-      '#134f5c'
-    ]
-  },
-  'Total': {
-    'min': 0,
-    'max': 20000,
-    'colors': [
-      '#d9d2e9',
-      '#b4a7d6',
-      '#8e7cc3',
-      '#674ea7',
-      '#351c75'
-    ]
-  }
-}
-
-function initialize() {
-  const sector = 'Residential';
-
-  const map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: new google.maps.LatLng(37.4, -119.8),
-    zoom: 5,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    zoomControlOptions: {
-      style: google.maps.ZoomControlStyle.SMALL
-    },
-  });
-
-  const layer = new google.maps.FusionTablesLayer();
-  updateLayerQuery(layer, sector);
-  layer.setMap(map);
-
-  createLegend(map, sector);
-  styleLayerBySector(layer, sector);
-  styleMap(map);
-  drawVisualization('Alameda');
-
-  google.maps.event.addListener(layer, 'click', function(e) {
-    const county = e.row['County'].value;
-    drawVisualization(county);
-
-    const electricity = e.row['2010'].value;
-    if (electricity > 5000) {
-      e.infoWindowHtml = '<p class="high">High Usage!</p>';
-    } else if (electricity > 2500) {
-      e.infoWindowHtml = '<p class="medium">Medium Usage</p>';
-    } else {
-      e.infoWindowHtml = '<p class="low">Low Usage</p>';
-    }
-  });
-
-  google.maps.event.addDomListener(document.getElementById('sector'),
-      'change', function() {
-        sector = this.value;
-        updateLayerQuery(layer, sector);
-        styleLayerBySector(layer, sector);
-        updateLegend(sector);
-      });
-
-  google.maps.event.addDomListener(document.getElementById('county'),
-      'change', function() {
-        const county = this.value;
-        updateLayerQuery(layer, sector, county);
-        drawVisualization(county);
-      });
-}
-
-function updateLayerQuery(layer, sector, county) {
-  const where = "Sector = '" + sector + "'";
-  if (county) {
-    where += " AND County = '" + county + "'";
-  }
-  layer.setOptions({
-    query: {
-      select: 'geometry',
-      from: '18fyPg1LvW3KB3N5DE_ub-MKicB0Nx7vkGn9kw4s',
-      where: where
-    }
-  });
-}
-
-function createLegend(map, sector) {
-  const legendWrapper = document.createElement('div');
-  legendWrapper.id = 'legendWrapper';
-  legendWrapper.index = 1;
-  map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(
-      legendWrapper);
-  legendContent(legendWrapper, sector);
-}
-
-function legendContent(legendWrapper, sector) {
-  const legend = document.createElement('div');
-  legend.id = 'legend';
-
-  const title = document.createElement('p');
-  title.innerHTML = sector + ' Electricity Consumption';
-  legend.appendChild(title);
-
-  const layerStyle = LAYER_STYLES[sector];
-  const colors = layerStyle.colors;
-  const minNum = layerStyle.min;
-  const maxNum = layerStyle.max;
-  const step = (maxNum - minNum) / colors.length;
-  for (let i = 0; i < colors.length; i++) {
-    var legendItem = document.createElement('div');
-
-    const color = document.createElement('div');
-    color.setAttribute('class', 'color');
-    color.style.backgroundColor = colors[i];
-    legendItem.appendChild(color);
-
-    const newMin = minNum + step * i;
-    const newMax = newMin + step;
-    const minMax = document.createElement('span');
-    minMax.innerHTML = newMin + ' - ' + newMax;
-    legendItem.appendChild(minMax);
-
-    legend.appendChild(legendItem);
-  }
-
-  legendWrapper.appendChild(legend);
-}
-
-function updateLegend(sector) {
-  const legendWrapper = document.getElementById('legendWrapper');
-  const legend = document.getElementById('legend');
-  legendWrapper.removeChild(legend);
-  legendContent(legendWrapper, sector);
-}
-
-function styleLayerBySector(layer, sector) {
-  const layerStyle = LAYER_STYLES[sector];
-  const colors = layerStyle.colors;
-  const minNum = layerStyle.min;
-  const maxNum = layerStyle.max;
-  const step = (maxNum - minNum) / colors.length;
-
-  const styles = new Array();
-  for (let i = 0; i < colors.length; i++) {
-    const newMin = minNum + step * i;
-    styles.push({
-      where: generateWhere(newMin, sector),
-      polygonOptions: {
-        fillColor: colors[i],
-        fillOpacity: 1
-      }
-    });
-  }
-  layer.set('styles', styles);
-}
-
-function generateWhere(minNum, sector) {
-  let whereClause = new Array();
-  whereClause.push("Sector = '");
-  whereClause.push(sector);
-  whereClause.push("' AND '2010' >= ");
-  whereClause.push(minNum);
-  return whereClause.join('');
-}
-
-function styleMap(map) {
-  const style = [{
-    featureType: 'all',
-    stylers: [{
-      saturation: -99
-    }]
-  }, {
-    featureType: 'poi',
-    stylers: [{
-      visibility: 'off'
-    }]
-  }, {
-    featureType: 'road',
-    stylers: [{
-      visibility: 'off'
-    }]
-  }];
-
-  const styledMapType = new google.maps.StyledMapType(style, {
-    map: map,
-    name: 'Styled Map'
-  });
-  map.mapTypes.set('map-style', styledMapType);
-  map.setMapTypeId('map-style');
-}
-
-function drawVisualization(county) {
-  google.visualization.drawChart({
-    containerId: "visualization",
-    dataSourceUrl: "http://www.google.com/fusiontables/gvizdata?tq=",
-    query: "SELECT Sector,'2006','2007','2008','2009','2010' " +
-        "FROM 18fyPg1LvW3KB3N5DE_ub-MKicB0Nx7vkGn9kw4s WHERE County = '" + county + "'",
-    chartType: "ColumnChart",
-    options: {
-      title: county,
-      height: 400,
-      width: 400
-    }
-  });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+function initMap(){
+  function e(){
+    var e="SELECT 'col0', 'London Terminus', 'Season Ticket Price', 'Commute Time', 'Average House Price', 'Trains Per Day', 'Crime Levels', 'Pubs Per Capita', 'Approx Population',  'Commuter Town Score' FROM 1lMyWyrSb6GyJWaZS8_oouid-YVs9vGEXs7-pKJud  ORDER BY 'Commuter Town Score' DESC LIMIT 10",o=encodeURIComponent(e),
+    a=new google.visualization.Query("https://www.google.com/fusiontables/gvizdata?tq="+o),
+    t={headerRow:"fusionTableRowHeader",tableRow:"fusionTableTableRows",oddTableRow:"fusionTableTableRows"};
+    a.send(function(e){
+      var o=new google.visualization.Table(document.getElementById("visualization")),
+      a=e.getDataTable(),
+      i=new google.visualization.NumberFormat({prefix:"£",negativeColor:"red",negativeParens:!0,fractionDigits:0});
+      i.format(a,2),i.format(a,4);
+      var s=new google.visualization.NumberFormat({suffix:" mins (fastest)",negativeColor:"red",negativeParens:!0,fractionDigits:0});
+      s.format(a,3);
+      var n=new google.visualization.NumberFormat({suffix:" out of 10",negativeColor:"red",negativeParens:!0,fractionDigits:2});
+      n.format(a,9),o.draw(a,{showRowNumber:!1,cssClassNames:t})})}
+      var o=new google.maps.Map(document.getElementById("map"),{center:{lat:51.5549458,lng:0},zoom:7,zoomControl:!0,zoomControlOptions:{position:google.maps.ControlPosition.LEFT_CENTER}}),
+      a=new google.maps.FusionTablesLayer({query:{select:"'col0'",from:"1lMyWyrSb6GyJWaZS8_oouid-YVs9vGEXs7-pKJud",where:"'Average House Price' < 1000000 AND 'Season Ticket Price' < 25000 AND 'Commute Time' < 200"},
+      options:{styleId:2,templateId:2}});
+      a.setMap(o),google.load("visualization","1",{packages:["table"],callback:e}),
+      google.maps.event.addDomListener(document.getElementById("sliders"),"input",
+      function(){
+        updateMap(a)}),
+        google.maps.event.addDomListener(document.getElementById("sliders"),"change",
+        function(){
+          updateMap(a)}),
+          google.maps.event.addListener(a,"click",function(e){
+            $(window).width()>1199&&o.panBy(300,100)})}function updateMap(e){
+              !function(){
+                var e="£"+$("#housePrice").val().toString().replace(/\B(?=(\d{3})+(?!\d))/g,","),o="£"+$("#seasonTicket").val().toString().replace(/\B(?=(\d{3})+(?!\d))/g,","),
+                a=$("#commuteTime").val()+" mins";1e6==$("#housePrice").val()?$("#housePriceNum").text(e+"+"):$("#housePriceNum").text(e),$("#seasonTicketNum").text(o),$("#commuteTimeNum").text(a)}();
+                var o={housePrice:$("#housePrice").val(),seasonTicket:$("#seasonTicket").val(),commuteTime:$("#commuteTime").val()},a=[],t="",i="",s="",n="",r="";$("input[type=checkbox]").each(
+                  function(){
+                    var e=$(this).attr("name"),o=$(this).attr("value");"station"===e&&$(this)[0].checked===!0&&0===a.length?a.push("'"+o+"'"):"station"===e&&$(this)[0].checked===!0&&a.length>0?a.push(" '"+o+"'"):"lowcrime"===e&&$(this)[0].checked===!0?t=" AND 'Crimes per 1000 of population' < 50":"goodprimaryschools"===e&&$(this)[0].checked===!0?n=" AND 'Good or Outstanding Primary Schools within 3 Miles' > 5":"goodsecondaryschools"===e&&$(this)[0].checked===!0?r=" AND 'Good or Outstanding Secondary Schools within 3 Miles' > 5":"pubs"===e&&$(this)[0].checked===!0?i=" AND 'Pubs Per Capita'='Higher than Average'":"airport"===e&&$(this)[0].checked===!0&&(s=" AND 'Distance from Nearest Airport' < 10")});
+                    var l="";a.length>0&&(l=" AND 'London Terminus' IN ("+a.toString()+")"),e.setOptions({query:{select:"'col0'",from:"1lMyWyrSb6GyJWaZS8_oouid-YVs9vGEXs7-pKJud",where:"'Average House Price' < "+o.housePrice+" AND 'Season Ticket Price' < "+o.seasonTicket+" AND 'Commute Time' < "+o.commuteTime+l+t+n+r+i+s}}),
+                    updateTable(o,l,t,n,r,i,s)}function updateTable(e,o,a,t,i,s,n){
+                      function r(){
+                        0==$(".google-visualization-table-td").length&&$("#personalised-results").css("display","none")}
+                        function l(){
+                          var l="SELECT 'col0', 'London Terminus', 'Season Ticket Price', 'Commute Time', 'Average House Price', 'Trains Per Day', 'Crime Levels', 'Pubs Per Capita', 'Approx Population',  'Commuter Town Score' FROM 1lMyWyrSb6GyJWaZS8_oouid-YVs9vGEXs7-pKJud WHERE 'Average House Price' < "+e.housePrice+" AND 'Season Ticket Price' < "+e.seasonTicket+" AND 'Commute Time' < "+e.commuteTime+o+a+t+i+s+n+" ORDER BY 'Commuter Town Score' DESC LIMIT 10",u=encodeURIComponent(l),c=new google.visualization.Query("https://www.google.com/fusiontables/gvizdata?tq="+u),m={headerRow:"fusionTableRowHeader",tableRow:"fusionTableTableRows",oddTableRow:"fusionTableTableRows"};$("#personalised-results").css("display","block"),
+                          c.send(function(e){
+                            var o=new google.visualization.Table(document.getElementById("visualization")),a=e.getDataTable(),
+                            t=new google.visualization.NumberFormat({prefix:"£",negativeColor:"red",negativeParens:!0,fractionDigits:0});t.format(a,2),t.format(a,4);var i=new google.visualization.NumberFormat({suffix:" mins (fastest)",negativeColor:"red",negativeParens:!0,fractionDigits:0});i.format(a,3);
+                            var s=new google.visualization.NumberFormat({suffix:" out of 10",negativeColor:"red",negativeParens:!0,fractionDigits:2});s.format(a,9),o.draw(a,{showRowNumber:!1,cssClassNames:m}),r()})}google.load("visualization","1",{packages:["table"],
+                            callback:l})}$("#filterShowHide").click(function(e){
+                              e.preventDefault(),$("#sliders").slideToggle("slow")});
